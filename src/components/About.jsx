3a27ebd1b6302002b94d3a2e2c1db59ef8202671
Sherryll Elliott <sherryllelliott@gmail.com>
2021-02-15 16:05:51 +0000

    about details added

---
+++ src/components/About.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Home = () => {
+
+  return <div role="main">
+    <h1>About</h1>
+    <h2> 
+      Project Management
+    </h2>
+    <p>
+      Fix It All aim to provide you with home improvement and 
+      property management services without the headaches. You tell
+      us what you need, we make it happen. No nonsense. No fuss. From 
+      Gas engineers, tilers and Plumbers to decorators and cleaners, We've
+      got you covered. 
+    </p>
+    <h3>"Good enough is never enough"</h3>
+    <p>
+      At Fix It All you will find experienced
+      and professional tradesmen with an eye for
+      quality and a passion for customer satisfaction. 
+      We do not accept anything short of perfection and
+      neither should you.
+    </p>
+   
+
+  </div>
+
+}
+
+export default Home
